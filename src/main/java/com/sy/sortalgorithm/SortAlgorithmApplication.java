c53ba3ee14a90ff0e96adc548c1baf5d8526afa1
孙宇 <1870971964@qq.com>
2024-02-03 22:40:28 +0800

    first commit

---
+++ src/main/java/com/sy/sortalgorithm/SortAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package com.sy.sortalgorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortAlgorithmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SortAlgorithmApplication.class, args);
+    }
+
+}
